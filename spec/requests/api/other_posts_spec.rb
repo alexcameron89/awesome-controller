8149279886f78ab7314b1d113bb743e0239d85d2
Alex Kitchens <alex.kitchens@stitchfix.com>
2020-03-29 21:26:39 -0500

    Simplify a test

--- spec/requests/api/other_posts_spec.rb
+++ spec/requests/api/other_posts_spec.rb
@@ -1,33 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Posts API (Rails Controller)", type: :request do
-  describe "GET /api/other_posts/1" do
-    let!(:post) { FactoryBot.create(:post) }
+describe "GET /api/other_posts/1" do
+  let(:post) { FactoryBot.create(:post) }
 
-    it 'returns the details of a post' do
-      get api_other_post_path(post)
+  it 'returns the details of a post' do
+    get api_other_post_path(post)
 
-      header_keys = ["ETag", "Cache-Control", "X-Request-Id", "X-Runtime", "Content-Length"]
-      expected_body = {
-        "post" =>  {
-          "id" => post.id,
-          "title" => post.title,
-          "author_id" => post.author_id,
-          "content" => post.content,
-          "created_at" => post.created_at.iso8601,
-          "updated_at" => post.updated_at.iso8601
-        }
-      }
-
-      body_details = JSON.parse(response.body)
-
-      aggregate_failures do
-        expect(response).to have_http_status(200)
-        expect(response.headers.keys).to include(*header_keys)
-        expect(body_details).to eq(expected_body)
-      end
+    aggregate_failures do
+      expect(response).to have_http_status(200)
+      expect(response.headers.keys).to_not eq(nil)
+      expect(response.body).to eq({ post: post }.to_json)
     end
   end
+end
 
   describe "POST /api/other_posts" do
     let(:title) { "Hello again!" }
