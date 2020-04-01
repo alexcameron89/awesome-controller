38247f6d3fdd6844349628cacf826b0f9796c9fc
Alex Kitchens <alex.kitchens@stitchfix.com>
2020-03-31 21:08:29 -0500

    Clean up some more of the rendering code

--- app/controllers/awesome_controller/rendering.rb
+++ app/controllers/awesome_controller/rendering.rb
@@ -1,10 +1,7 @@
 module AwesomeController
   module Rendering
-    # Performs the actual template rendering.
-    def render_to_body(options = {})
-      super
-    end
-
+    extend ActiveSupport::Concern
+    include ActionView::Rendering
     # Stolen from action_controller/metal/rendering.rb
     # Process controller specific options, as status, content-type and location.
     def _process_options(options)
@@ -13,6 +10,32 @@ module AwesomeController
       response.status = status if status
       response.content_type = content_type if content_type
       headers["Location"] = url_for(location) if location
+    end
+
+    #    Failures:
+    #
+    #  1) Creating and Viewing Posts (Custom Controller) creating posts allows a user to create a post
+    #     Failure/Error: super
+    #
+    #     NameError:
+    #       undefined local variable or method `view_assigns' for #<OtherPostsController:0x00007f811b1ac838>
+    #       Did you mean?  view_paths
+    #     # ./dependencies/rails/actionview/lib/action_view/rendering.rb:92:in `view_context'
+    #     # ./dependencies/rails/actionview/lib/action_view/rendering.rb:111:in `_render_template'
+    #     # ./dependencies/rails/actionview/lib/action_view/rendering.rb:103:in `render_to_body'
+    #     # ./app/controllers/awesome_controller/implicit_rendering.rb:63:in `render_to_body'
+    #     # ./app/controllers/awesome_controller/base.rb:11:in `render_to_body'
+    #     # ./app/controllers/awesome_controller/implicit_rendering.rb:33:in `render'
+    #     # ./app/controllers/awesome_controller/implicit_rendering.rb:25:in `default_render'
+    #     # ./app/controllers/awesome_controller/implicit_rendering.rb:14:in `process_action'
+    #     # ./app/controllers/awesome_controller/basest_of_base.rb:53:in `dispatch'
+    #     # ./app/controllers/awesome_controller/basest_of_base.rb:44:in `dispatch'
+    #
+    #     Original code: abstract_controller/rendering.rb
+    def view_assigns
+      instance_variables.each_with_object({}) { |name, hash|
+        hash[name.slice(1, name.length)] = instance_variable_get(name)
+      }
     end
 
     # This is similar to an initializer for ActionController in the Rails engine:
